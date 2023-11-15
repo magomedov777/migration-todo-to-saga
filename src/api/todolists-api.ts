ceae5797f751ed382aca5637966e1c0f94750fe7
Arkadiy <alibullterrier@gmail.com>
2023-11-15 05:45:32 +0200

    create: file for enums, add to index

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios'
+import { TaskPriorities, TaskStatuses } from '../utils';
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
@@ -48,22 +49,6 @@ export type ResponseType<D = {}> = {
     messages: Array<string>
     fieldsErrors: Array<string>
     data: D
-}
-
-
-export enum TaskStatuses {
-    New = 0,
-    InProgress = 1,
-    Completed = 2,
-    Draft = 3
-}
-
-export enum TaskPriorities {
-    Low = 0,
-    Middle = 1,
-    Hi = 2,
-    Urgently = 3,
-    Later = 4
 }
 
 export type TaskType = {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './handle-server-network-error-saga';
 export * from './handle-server-app-error-saga';
+export * from './enums';
